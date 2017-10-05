747d96f4959a2d4c90437d9123d970045081d497
Дмитрий Дудин <svetoz@gmail.com>
2017-10-05 14:03:42 +0300

    Test Answer controller for create

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'POST #create' do
+    let(:valid_params) { { answer: attributes_for(:answer), question_id: question } }
+    let(:invalid_params_with_question) { { answer: attributes_for(:invalid_answer), question_id: question } }
+    let(:invalid_params_by_nil_question) { { answer: attributes_for(:answer), question_id: nil } }
+
+    context 'with valid attribute' do
+      it 'saves the new answer' do
+        expect { post :create, params: valid_params }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirect to associates question' do
+        post :create, params: valid_params
+        expect(response).to redirect_to question_path(assign(question))
+      end
+    end
+
+    context 'with invalid attribute' do
+      it 'does not save the answer' do
+        expect { post :create, params: invalid_params_with_question }.to_not change(Question, :count)
+      end
+      it 'does not save the answer without question' do
+        expect { post :create, params: invalid_params_by_nil_question }.to_not change(Question, :count)
+      end
+      it 're-render associates question view' do
+        post :create, params: { answer: attributes_for(:answer) }
+        expect(response).to render_template(question_path(assign(question)))
+      end
+    end
+  end
 end
