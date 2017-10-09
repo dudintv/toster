6555132b47362105e5870ad8d9e187937c762911
Дмитрий Дудин <svetoz@gmail.com>
2017-10-09 21:42:25 +0300

    Test of sign up

--- spec/support/features_helper.rb
+++ spec/support/features_helper.rb
@@ -1,4 +1,12 @@
 module FeaturesHelper
+  def sign_up(user)
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Пароль', with: user.password
+    fill_in 'Подтверждение пароля', with: user.password
+    click_on 'Регистрация'
+  end
+
   def sign_in(user)
     visit new_user_session_path
     fill_in 'Email', with: user.email
