e61e1d2fd0d17017b9cf1cd4c176bea71e07639e
Дмитрий Дудин <svetoz@gmail.com>
2017-10-10 19:37:36 +0300

    Make delete answers

--- spec/features/answer/delete_answer_spec.rb
+++ spec/features/answer/delete_answer_spec.rb
@@ -7,35 +7,39 @@ feature 'Delete answer', %q{
 } do
 
   before do
-    @user = create(:user)
-    @question = create(:question)
     @my_answer = create(:answer)
-    @user.answers << @my_answer
-    @question.answers << @my_answer
     @foreign_answer = create(:answer)
-    @question.answers << @foreign_answer
+
+    @user = create(:user)
+    @user.answers << @my_answer
+
+    @question = create(:question)
+    @question.answers << @my_answer
+
+    @foreign_question = create(:question)
+    @foreign_question.answers << @foreign_answer
   end
-  
+
   scenario 'Authenticated user deletes own answer' do
     sign_in(@my_answer.user)
     visit question_path(@my_answer.question)
     click_on 'Удалить ответ'
 
     expect(page).to have_content 'Ответ успешно удален.'
-    expect(page).to_not have_content @my_answer.title
-    expect(current_path).to eq answers_path
+    expect(page).to_not have_content @my_answer.body
+    expect(current_path).to eq question_path(@my_answer.question)
   end
 
   scenario 'Guest user can not delete answer' do
     visit question_path(@my_answer.question)
-
+    
     expect(page).to_not have_content 'Удалить ответ'
   end
 
   scenario 'Authenticated user try deletes foreign answer' do
     sign_in(@user)
     visit question_path(@foreign_answer.question)
-    
+
     expect(page).to_not have_content 'Удалить ответ'
   end
 end
