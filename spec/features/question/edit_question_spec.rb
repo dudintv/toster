073705dd8ab0bb74de41b82a09a9db9cc0037706
Дмитрий Дудин <svetoz@gmail.com>
2017-10-26 22:51:09 +0300

    Feature test other actions edit question

--- spec/features/question/edit_question_spec.rb
+++ spec/features/question/edit_question_spec.rb
@@ -8,6 +8,7 @@ feature 'Edit question', '
 
   given(:user) { create(:user) }
   given(:my_question) { create(:question, user: user) }
+  given(:foreign_question) { create(:question) }
 
   scenario 'Author of question edit own question', js: true do
     sign_in(user)
@@ -32,5 +33,33 @@ feature 'Edit question', '
       expect(page).to have_content 'Обновленный вопрос'
       expect(page).to have_content 'Обновленные подробности'
     end
+  end
+
+  scenario 'Author of question can not update question with invalid data', js: true do
+    sign_in(user)
+    visit question_path(my_question)
+
+    within '#question-block' do
+      click_on 'Редактировать вопрос'
+      fill_in 'Вопрос', with: ''
+      fill_in 'Подробности', with: ''
+      click_on 'Сохранить Вопрос'
+
+      expect(page).to have_content 'Вопрос не может быть пустым'
+      expect(page).to have_content 'Подробности вопроса не может быть пустым'
+    end
+  end
+
+  scenario 'Non-author can`t edit question' do
+    sign_in(user)
+    visit question_path(foreign_question)
+
+    expect(page).to_not have_link 'Редактировать вопрос'
+  end
+
+  scenario 'Guest can`t edit question' do
+    visit question_path(foreign_question)
+
+    expect(page).to_not have_link 'Редактировать вопрос'
   end
 end
