00e321c7cace4a2cb03d16084fc86066c4165b5f
Дмитрий Дудин <svetoz@gmail.com>
2017-10-30 22:29:03 +0300

    Test of full cleanuping new-answer-form when create new answer

--- spec/features/answer/attach_files_to_answer_spec.rb
+++ spec/features/answer/attach_files_to_answer_spec.rb
@@ -17,8 +17,12 @@ feature 'Attach files to Answer', '
     attach_file 'answer[attachments_attributes][0][file]', "#{Rails.root}/spec/rails_helper.rb"
     click_on 'Создать Ответ'
 
+    eventually do
+      expect(page.find('#new-answer-form').find_field('answer[attachments_attributes][0][file]').value).to eq ''
+    end
+
     within('#answers') do
-      expect(page).to have_link('rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb') 
+      expect(page).to have_link('rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb')
     end
   end
 end
