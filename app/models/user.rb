0df24f05ec952c9aa6cfb65a0b6edf19783e92cf
Дмитрий Дудин <svetoz@gmail.com>
2017-11-22 20:29:40 +0300

    Create Authorization model and test it

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   def author_of?(obj)
     obj&.user_id == id
