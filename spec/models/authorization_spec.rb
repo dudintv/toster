31e92aed7deeeb20a38869816867909f503f54a3
Дмитрий Дудин <svetoz@gmail.com>
2017-12-06 00:54:27 +0300

    Test after_create callback Authorization model

--- spec/models/authorization_spec.rb
+++ spec/models/authorization_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Authorization, type: :model do
   it { should belong_to(:user) }
+
+  describe '#after_create' do
+    let(:authorization) { create(:authorization) }
+
+    it 'confirmation_token is set after create' do
+      expect(authorization.confirmation_token).not_to be_nil
+    end
+    it 'confirmation_token have 20 characters or more' do
+      expect(authorization.confirmation_token.length).to be >= 20
+    end
+  end
 end
