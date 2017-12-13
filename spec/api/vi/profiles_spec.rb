c3bb2f17531a6485bfc7fe1856e2b21fd1e9f8e9
Дмитрий Дудин <svetoz@gmail.com>
2017-12-13 14:32:08 +0300

    Test API returns attrs from /other

--- spec/api/vi/profiles_spec.rb
+++ spec/api/vi/profiles_spec.rb
@@ -37,4 +37,46 @@ describe 'Profile API' do
       end
     end
   end
+
+  describe 'GET /others' do
+    context 'unauthorized' do
+      it 'return 401 status if there is no access_token' do
+        get '/api/v1/profiles/others', params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it 'return 401 status if access_token is invalid' do
+        get '/api/v1/profiles/others', params: { format: :json, access_token: '123' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:me) { create :user }
+      let!(:others) { create_list(:user, 2) }
+      let(:access_token) { create :access_token, resource_owner_id: me.id }
+
+      before { get '/api/v1/profiles/others', params: { format: :json, access_token: access_token.token } }
+
+      it 'returm 200 status' do
+        expect(response).to be_success
+      end
+
+      it 'no contains current_user' do
+        expect(response.body).to be_json_eql(others.to_json)
+      end
+
+      %w(id email created_at updated_at).each do |attr|
+        it "contains #{attr}" do
+          expect(response.body).to be_json_eql(others.first.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "not contains #{attr}" do
+          expect(response.body).to_not have_json_path(attr)
+        end
+      end
+    end
+  end
 end
