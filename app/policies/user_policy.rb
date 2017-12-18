d126b907c554f11307deacb39c52757788d71b9c
Дмитрий Дудин <svetoz@gmail.com>
2017-12-18 18:59:44 +0300

    Lottle refactor User's policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -9,35 +9,23 @@ class UserPolicy < ApplicationPolicy
     true
   end
 
-  def show?
-    true
-  end
-
-  def new?
-    true
-  end
-
-  def create?
-    true
-  end
-
   def edit?
     user.id == record.id
   end
 
   def update?
-    user.id == record.id
+    edit?
   end
 
   def destroy?
-    user.id == record.id
+    edit?
   end
 
   def me?
-    user.present?
+    user
   end
 
   def others?
-    user.present?
+    user
   end
 end
