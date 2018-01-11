7d2dbe363efa23dc02f55ddb6a70e4a04e134f66
Дмитрий Дудин <svetoz@gmail.com>
2018-01-11 21:06:57 +0300

    add dotenv string into application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module Toster
   class Application < Rails::Application
     # Use the responders controller from the responders gem
